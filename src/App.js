0fba5ddbb40340471e99e3273ddb3da451307df3
Winston Nguyen <149917453+nwins372@users.noreply.github.com>
2025-04-26 16:11:16 -0700

    restructure app.js

--- src/App.js
+++ src/App.js
@@ -3,35 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-    <div id="app-container">
-       <NavBar />
-      <header>
-      </header>
-
-      <div className="container">
-        <main>
-          <h1>Top Story: Your Team Wins Big!</h1>
-          <p>Angels geting swept in the series. Is this really a suprise?</p>
-        </main>
-
-        <aside>
-          <h2>Latest Scores</h2>
-          <ul>
-            <li>Yankees 5 - Red Sox 3</li>
-            <li>Lakers 102 - Celtics 99</li>
-            <li>Cowboys 24 - Giants 20</li>
-          </ul>
-        </aside>
-      </div>
-
-      <footer>
-        <p>&copy; Sports Predictor. All rights reserved.</p>
-      </footer>
-    </div>
+    <Home />
     </BrowserRouter>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import NavBar from "../components/NavBar";
+import { BrowserRouter } from "react-router-dom";
+
+function Home() {
+    return (
+        <>
+        <div id="app-container">
+        <NavBar />
+       <header>
+       </header>
+ 
+       <div className="container">
+         <main>
+           <h1>Top Story: Your Team Wins Big!</h1>
+           <p>Angels geting swept in the series. Is this really a suprise?</p>
+         </main>
+ 
+         <aside>
+           <h2>Latest Scores</h2>
+           <ul>
+             <li>Yankees 5 - Red Sox 3</li>
+             <li>Lakers 102 - Celtics 99</li>
+             <li>Cowboys 24 - Giants 20</li>
+           </ul>
+         </aside>
+       </div>
+ 
+       <footer>
+         <p>&copy; Sports Predictor. All rights reserved.</p>
+       </footer>
+     </div>
+     </>
+    )
+}
+
+export default Home;
