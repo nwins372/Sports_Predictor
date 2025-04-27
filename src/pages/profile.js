9247f66a3ff04df7cd8b9fc0387012e5eefb7345
smitty* <115043903+smitty28-csulb@users.noreply.github.com>
2025-04-26 22:19:19 -0700

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -7,13 +7,11 @@ function Profile_Page() {
         <NavBar />    
         <div className="profile-container">
         <h1>User Profile</h1>
-      <div className="login-box">
-        <form className="login-form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit" id="login-button">Log In</button>
-          <button type="button" id="create-account-button">Create Account</button>
-        </form>
+      <div className="profile-box">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" id="login-button">Log In</button>
+        <button type="button" id="create-account-button">Create Account</button>
       </div>
     </div>
     </>
