72f25563c5040ba057c931688d7b4d9696f7fc62
smitty* <115043903+smitty28-csulb@users.noreply.github.com>
2025-04-26 22:03:14 -0700

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,2 +1,23 @@
 import NavBar from '../components/NavBar';
 import './Login.css';
+
+function Profile_Page() {
+    return (
+        <>
+        <NavBar />    
+        <div className="profile-container">
+        <h1>User Profile</h1>
+      <div className="login-box">
+        <form className="login-form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit" id="login-button">Log In</button>
+          <button type="button" id="create-account-button">Create Account</button>
+        </form>
+      </div>
+    </div>
+    </>
+    );
+}
+
+export default Profile_Page;
